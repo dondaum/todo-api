9de14cd14b0191fcb478a991e42eed4c71c04293
dondaum <sebastian.daum89@gmail.com>
2018-06-17 15:53:09 +0200

    initial commit and setup for react app

---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+todos = Todo.create([
+    {
+        title: 'Train sailing',
+        due_date: '20.03.2017',
+        priority: 'low'
+    },
+    {
+        title: 'Be a very good man',
+        due_date: '10.07.2019',
+        priority: 'high'
+    },
+    {
+        title: 'Wash the dishes',
+        due_date: '20.03.2018',
+        priority: 'low'
+    }
+])
